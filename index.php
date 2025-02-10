6e4e2c82d3fe817a3584097f4b606c2b998f8233
rovy-saputra-nugeraha <2101020012@student.umrah.ac.id>
2025-02-10 13:49:01 +0700

    Belajar PHP Dasar 1

--- index.php
+++ index.php
@@ -1,8 +1,64 @@
 <?php
-  // var_dump("Rovy Saputra")
+// Belajar PHP Dasar 1
 
-  $nama = "Rovy Saputra Nugeraha";
-  
+//=========================
+// SINTAKS PHP
+// Standar Output
+// echo, print
+// print_r
+// var_dump
 
+//=========================
+// PENULISAN SINTAKS PHP
+// 1. PHP di dalam HTML (Recomendasi)
+// 2. HTML di dalam PHP
+
+//=========================
+//VARIABEL DAN TIPE DATA
+//Variabel {Tidak boleh diawali oleh angka, tapi boleh mengandung angka}.
+// $nama = "Rovy Saputra";
+// echo "Hallo, Nama Saya $nama";
+
+//=========================
+// OPERATOR 
+// Aritmatika
+// +, -, * , /, %
+// $x = "10";
+// $y = "20";
+// echo $x + $y;
+
+//=========================
+// Penggabungan String / Concatenation / Concat
+// . 
+// $nama_depan = "Rovy Saputra";
+// $nama_belakang = "Nugeraha";
+// echo $nama_depan. " ". $nama_belakang;
+
+//=========================
+//ASSIGMENT
+// =, +=, -=, /=, .=
+// $x = 1;
+// $x += 5;
+// echo $x;
+
+//=========================
+// $nama =  "Rovy Saputra";
+// $nama .= " ";
+// $nama .= "Nugeraha";
+// echo $nama;
+
+//=========================
+//PERBANDINGAN
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+//Identitas
+//===, !==
+// var_dump(1 === "1");
+
+//=========================
+//LOGIKA
+// &&, ||, !
+// $x = 30;
+// var_dump($x < 20 && $x % 2 == 0);
 
 ?>
