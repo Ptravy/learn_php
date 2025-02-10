1b52f07f814121b244be2a0400c4edb6f6a908ef
rovy-saputra-nugeraha <2101020012@student.umrah.ac.id>
2025-02-10 14:05:26 +0700

    Perulangan pada PHP

---
+++ Perulangan.php
@@ -0,0 +1,25 @@
+<?php
+// for
+// for( $i = 0; $i < 5; $i++ ){
+//   echo "Hello Rovy <br>";
+// }
+
+//=========================
+//while
+// $i = 0;
+// while( $i < 10 ){
+//   echo "Hello Rovy <br>";
+//   $i++; // Increment atau Decrement
+// }
+
+//=========================
+// do..while
+// $i = 0;
+// do{
+//   echo "Hello Rovy <br>";
+//   $i++;
+// }while( $i < 5 );
+
+//foreach : pengulangan khusus array
+?>
+
