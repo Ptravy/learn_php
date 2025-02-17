57e72eee27596d4612915ae162a6bab324071e04
Ptravy <rovysaputra10@gmail.com>
2025-02-17 16:51:19 +0700

    Belajar Update Database

---
+++ UPDATEDATA/function.php
@@ -0,0 +1,67 @@
+<?php
+ // Koneksi Database
+ $conn = mysqli_connect("localhost", "root", "", "movie");
+
+function query($query){
+  global $conn;
+  $result = mysqli_query($conn, $query);
+
+  $rows = [];
+  while($row = mysqli_fetch_assoc($result)){
+    $rows[] = $row;
+  }
+  return $rows;
+}
+
+function tambah($data){
+  global $conn;
+  // ambil data dari tiap elemen dalam form
+  $judul = htmlspecialchars($data["judul"]);
+  $genre = htmlspecialchars($data["genre"]);
+  $sutradara = htmlspecialchars($data["sutradara"]);
+  $tahun = htmlspecialchars($data["tahun"]);
+  $gambar = htmlspecialchars($data["gambar"]);
+
+   //query insert data
+   $query = "INSERT INTO film
+   VALUES
+  ('', '$judul', '$genre', '$sutradara', '$tahun', '$gambar')
+  "; 
+mysqli_query($conn, "$query");
+
+return mysqli_affected_rows($conn);
+
+}
+
+function hapus($id){
+  global $conn;
+  mysqli_query($conn, "DELETE FROM film WHERE id = $id");
+
+  return mysqli_affected_rows($conn);
+}
+
+function ubah($data){
+    global $conn;
+    // ambil data dari tiap elemen dalam form
+    $id = $data["id"];
+    $judul = htmlspecialchars($data["judul"]);
+    $genre = htmlspecialchars($data["genre"]);
+    $sutradara = htmlspecialchars($data["sutradara"]);
+    $tahun = htmlspecialchars($data["tahun"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+
+    //query update data
+    $query = "UPDATE film SET
+              judul = '$judul',
+              genre = '$genre',
+              sutradara = '$sutradara',
+              tahun = '$tahun',
+              gambar = '$gambar' 
+              WHERE id = $id
+    "; 
+  mysqli_query($conn, "$query");
+
+  return mysqli_affected_rows($conn);
+
+}
+?>
---
+++ UPDATEDATA/index.php
@@ -0,0 +1,49 @@
+<?php
+ require 'function.php';
+ 
+ $film = query("SELECT * FROM film");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Halaman Admin</title>
+</head>
+<body>
+  <h1>Daftar Film</h1>
+
+  <a href="tambah.php">Tambah Data Film</a>
+  <br><br>
+
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>No</th>
+      <th>Cover</th>
+      <th>Judul</th>
+      <th>Genre</th>
+      <th>Sutradara</th>
+      <th>Tahun</th>
+      <th>Aksi</th>
+    </tr>
+
+    <?php $i = 1; ?>
+    <?php foreach($film as $row) : ?>
+    <tr>
+      <td><?= $i; ?></td>
+      <td><img src="../Associative_Array/img/<?= $row["gambar"]; ?>" alt="gambar" width="50" height="50"></td>
+      <td><?= $row["judul"]; ?></td>
+      <td><?= $row["genre"]; ?></td>
+      <td><?= $row["sutradara"]; ?></td>
+      <td><?= $row["tahun"]; ?></td>
+      <td>
+        <a href="update.php?id=<?= $row["id"]; ?>">Edit</a> | 
+        <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('Yakin?')">Delete</a>
+      </td>
+    </tr>
+    <?php $i++; ?>
+    <?php endforeach; ?>
+  </table>
+</body>
+</html>
